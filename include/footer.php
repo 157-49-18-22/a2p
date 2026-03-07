9510253b2d5480529ed2269d5b49b2853717dfca
Abhi <576abhishek.2020@gmail.com>
2026-03-07 16:20:15 +0530

    all before this is good

--- include/footer.php
+++ include/footer.php
@@ -1291,6 +1291,7 @@ function handleKeyPress(e) {
 
 
 <script src="<?= SITE_URL; ?>assets/js/ambed.js"></script>
+<script src="<?= SITE_URL; ?>assets/js/analytics-tracker.js"></script>
 
 <script src="<?= SITE_URL; ?>assets/vendors/toolbar/js/js.cookie.min.js"></script>
 <script src="<?= SITE_URL; ?>assets/vendors/toolbar/js/jQuery.style.switcher.min.js"></script>
