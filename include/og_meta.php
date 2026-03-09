ed5acd6ffd98930c51867b898a69fd27d3e01b01
Abhi <576abhishek.2020@gmail.com>
2026-03-09 17:27:03 +0530

    new

--- include/og_meta.php
+++ include/og_meta.php
@@ -6,8 +6,10 @@
 $_protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
 $_host = $_SERVER['HTTP_HOST'];
 $_path = $_SERVER['REQUEST_URI'];
-// Add ?v=1.1 to force WhatsApp to refresh the image cache
-$_full_logo_url = $_protocol . "://" . $_host . "/upload/290126125406LOGO.png";
+// Square image (512x512) for best Android + iOS + Desktop compatibility
+$_full_logo_url = $_protocol . "://" . $_host . "/assets/images/favicons/android-chrome-512x512.png";
+$_logo_width    = "512";
+$_logo_height   = "512";
 $_current_page_url = $_protocol . "://" . $_host . $_path;
 ?>
 <!-- Open Graph / WhatsApp Preview Tags -->
@@ -18,8 +20,8 @@ $_current_page_url = $_protocol . "://" . $_host . $_path;
 <meta property="og:image" content="<?php echo $_full_logo_url; ?>">
 <meta property="og:image:secure_url" content="<?php echo $_full_logo_url; ?>">
 <meta property="og:image:type" content="image/png">
-<meta property="og:image:width" content="600">
-<meta property="og:image:height" content="600">
+<meta property="og:image:width" content="<?php echo $_logo_width; ?>">
+<meta property="og:image:height" content="<?php echo $_logo_height; ?>">
 <meta property="og:type" content="website">
 
 <!-- Twitter Card -->
