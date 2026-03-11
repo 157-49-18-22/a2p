7f701d93a10888723406f6e48773460c8884ccc6
Abhi <576abhishek.2020@gmail.com>
2026-03-11 16:01:54 +0530

    new

--- service_detail.php
+++ service_detail.php
@@ -41,8 +41,11 @@ if (count($sql_ser)) {
             
             <meta property="og:title" content="<?php echo htmlspecialchars($subproductss['meta_title']); ?>">
             <meta property="og:description" content="<?php echo htmlspecialchars($subproductss['meta_description']); ?>">
-            <meta property="og:image" content="<?= SITE_URL; ?>upload/290126125406LOGO.png?v=1.4">
-            <meta property="og:image:secure_url" content="<?= SITE_URL; ?>upload/290126125406LOGO.png?v=1.4">
+            <?php 
+            $_og_img = !empty($subproductss['photo']) ? SITE_URL . "upload/" . trim($subproductss['photo']) : SITE_URL . "upload/290126125406LOGO.png";
+            ?>
+            <meta property="og:image" content="<?php echo $_og_img; ?>?v=1.2">
+            <meta property="og:image:secure_url" content="<?php echo $_og_img; ?>?v=1.2">
             <meta property="og:image:width" content="600">
             <meta property="og:image:height" content="315">
             <meta property="og:url" content="<?php echo SITE_URL . ltrim($_SERVER['REQUEST_URI'], '/'); ?>">
@@ -52,7 +55,7 @@ if (count($sql_ser)) {
             <meta name="twitter:card" content="summary_large_image">
             <meta name="twitter:title" content="<?php echo htmlspecialchars($subproductss['meta_title']); ?>">
             <meta name="twitter:description" content="<?php echo htmlspecialchars($subproductss['meta_description']); ?>">
-            <meta name="twitter:image" content="<?= SITE_URL; ?>upload/290126125406LOGO.png?v=1.4">
+            <meta name="twitter:image" content="<?php echo $_og_img; ?>?v=1.2">
 
             
             
